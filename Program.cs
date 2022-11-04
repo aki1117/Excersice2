a63dafe8b858d1bd6fbdeb10f5a3c991fc34f603
Abdul Eriawan <abdul.eriawan.ft18@mail.umy.ac.id>
2022-11-04 16:01:57 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Excersice2
+{
+    class Program
+    {
+        public int[] Algorithm(int[] arya, int low, int high){
+            if(low >= high)
+            {
+                int mid = (low + high) / 2;
+                Algorithm(arya, low, mid);
+                Algorithm(arya, mid + 1, high);
+            }
+
+            return arya;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
